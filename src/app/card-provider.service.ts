187d9a7efae1e229e6048b2bfce2183b53f239ef
natron2244 <natron2244@gmail.com>
2020-06-02 09:05:12 -0300

    Adding alexs cards

--- src/app/card-provider.service.ts
+++ src/app/card-provider.service.ts
@@ -3,9 +3,9 @@ import { CardProvider } from "./interfaces";
 
 // Generated with https://csvjson.com/csv2json as hash
 import * as NathansCards from "./player-data/nathan.json";
-import * as OtherPlayer from "./player-data/example-player.json";
-import * as Dustin from "./player-data/dustin.json";
-import { from } from "rxjs";
+import * as AlexsCards from "./player-data/alex.json";
+// import * as OtherPlayer from "./player-data/example-player.json";
+// import * as Dustin from "./player-data/dustin.json";
 
 @Injectable({
   providedIn: "root",
@@ -22,16 +22,31 @@ export class CardProviderService {
     return [
       {
         name: "Nathan",
-        cards: NathansCards.cards,
+        cards: this.cleanCardData(NathansCards.cards),
       },
       {
-        name: "Other",
-        cards: OtherPlayer.cards,
+        name: "Alex",
+        cards: this.cleanCardData(AlexsCards.cards),
       },
-      {
-        name: "Dustin",
-        cards: Dustin.cards,
-      },
+      // {
+      //   name: "Other",
+      //   cards: OtherPlayer.cards,
+      // },
+      // {
+      //   name: "Dustin",
+      //   cards: Dustin.cards,
+      // },
     ];
+  }
+
+  private cleanCardData(cards: any): any[] {
+    let cleanedCards = cards;
+    const basicLands = ["Forest", "Island", "Mountain", "Swamp", "Plains"];
+    basicLands.forEach((basicLand) => (cleanedCards[basicLand] = undefined));
+
+    // const token = "token";
+    // cleanedCards = cleanedCards.filter((card) => card.type !== token);
+
+    return cleanedCards;
   }
 }
